225347412fcf2146ffffc3ef729077e16b79b92c
Julia Melnik <yuliyamielnik@gmail.com>
2020-06-09 20:58:21 +0200

    add route to app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import MainLayout from './components/layout/MainLayout/MainLayout';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/views/Dashboard/Dashboard';
+import Login from './components/views/Login/Login';
+import Kitchen from './components/views/Kitchen/Kitchen';
+import Ordering from './components/views/Ordering/Ordering';
+import Tables from './components/views/Tables/Tables';
 
 
 function App() {
   return (
+    <BrowserRouter basename={'/panel'}>
     <MainLayout>
-      <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Switch>
+        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
+        <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+        <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+        <Route exact path={process.env.PUBLIC_URL + '/ordering'} component={Ordering} />
+        <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+      </Switch>
     </MainLayout>
+  </BrowserRouter>
     
   );
 }
