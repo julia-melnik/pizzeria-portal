16790ea1cf2c38076b5049635745ce75ebc84e24
Julia Melnik <yuliyamielnik@gmail.com>
2020-06-15 19:08:16 +0200

    chenge links to tables/orders page

--- src/components/views/Ordering/Ordering.js
+++ src/components/views/Ordering/Ordering.js
@@ -3,23 +3,38 @@ import styles from './Ordering.scss';
 import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import { Container } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core/styles';
 
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 export default function Ordering() {
-return (
-  <div className={styles.component} >
-    <Container >
-      <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-      Ordering view
+  const classes = useStyles();
+  return (
+    <div className={styles.root} >
+      <Container >
+        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+          Ordering view
         </Typography>
         <div>
-      <Link to={`${process.env.PUBLIC_URL}/ordering/new`}>New Order</Link>
-      <Link to={`${process.env.PUBLIC_URL}/ordering/order/:id`}>Order id</Link>
-    </div> 
-    </Container>
+          <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group" align="center">
+            <Button> <Link to={`${process.env.PUBLIC_URL}/ordering/new`}>New Order</Link></Button>
+            <Button> <Link to={`${process.env.PUBLIC_URL}/ordering/order/:id`}>Order id</Link></Button>
+          </ButtonGroup>
+        </div>
+      </Container>
 
-  </div>
-);
-  
+    </div>
+  );
+
 }
--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -3,20 +3,42 @@ import styles from './Tables.scss';
 import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import { Container } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 export default function Tables() {
+  const classes = useStyles();
+
   return (
-    <div className={styles.component} >
+    <div className={styles.root} >
       <Container >
         <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
           Tables view
         </Typography>
         <div>
-          <Link to={`${process.env.PUBLIC_URL}/tables/booking/:id`}>Tables Booking</Link>
-          <Link to={`${process.env.PUBLIC_URL}/tables/event/:id`}>Tables event</Link>
+          <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group" align="center">
+            <Button>  <Link to={`${process.env.PUBLIC_URL}/tables/booking/:id`}>Tables Booking</Link></Button>
+            <Button><Link to={`${process.env.PUBLIC_URL}/tables/event/:id`}>Tables event</Link></Button>
+          </ButtonGroup>
         </div>
       </Container>
     </div>
   );
 }
+
+
+
+
 
